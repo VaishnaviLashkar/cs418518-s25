d5fe06c0f488301456a487979c4ecfde52396405
Vaishnavi Lashkar <vaishnavilashkar27@gmail.com>
2025-04-21 22:31:44 -0400

    updated code

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL = "https://cs418518-s25-production.up.railway.app/api";
 
 export const getUsers = async (email) => {
 
